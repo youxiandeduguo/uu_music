6a52b4286a44024566aadbf36b4c8625e49e1faa
youxiandeduguo <2932615926@qq.com>
2025-01-26 12:06:23 +0800

    导航栏完工

--- src/App.vue
+++ src/App.vue
@@ -14,27 +14,36 @@
         size="large"
         placeholder="搜索音乐、MV、歌单、用户"
         :suffix-icon="Search"
-      />
-
-      <el-button class="loginbutton">登录</el-button>
-      <el-select
-        v-model="value"
-        placeholder="开通VIP"
-        size="large"
-        class="vipbutton"
-        style="width: 120px"
-      >
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
         />
-      </el-select>
-
-      <button>充值</button>
-
       </div>
+      <div class="loginframe">
+        <el-button class="loginbutton">登录</el-button>
+      </div>
+      
+      <el-dropdown class="VIPbutton">
+          <el-button type="primary" class="vip-button">
+            开通VIP<el-icon class="el-icon--right"><arrow-down /></el-icon>
+          </el-button>
+          <template #dropdown>
+            <el-dropdown-menu class="custom-dropdown">
+              <el-dropdown-item>开通超级会员</el-dropdown-item>
+              <el-dropdown-item>开通豪华绿钻</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+      </el-dropdown>
+
+      <el-dropdown class="buybutton">
+        <el-button type="primary" class="buy-button">
+          充值<el-icon class="el-icon--right"><arrow-down /></el-icon>
+        </el-button>
+        <template #dropdown>
+          <el-dropdown-menu class="custom-dropdown">
+            <el-dropdown-item>购买乐币</el-dropdown-item>
+            <el-dropdown-item>充值饭票</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+
     </div>
     <RouterView class="view"></RouterView>
   </div>
@@ -46,6 +55,7 @@
   import { ref } from 'vue'
   import {RouterView,RouterLink} from 'vue-router'
   import { Search } from '@element-plus/icons-vue'
+  import { ArrowDown } from '@element-plus/icons-vue'
   export default {
     name:'app'
   }
@@ -57,7 +67,7 @@
   const input = ref('')
   const value = ref('')
 
-  const options = [
+  const options1 = [
     {
       value: 'Option1',
       label: '开通超级会员',
@@ -65,6 +75,16 @@
     {
       value: 'Option2',
       label: '开通绿钻豪华版',
+    },
+  ]
+  const options2 = [
+    {
+      value: 'Option1',
+      label: '购买乐币',
+    },
+    {
+      value: 'Option2',
+      label: '充值饭票',
     },
   ]
 
@@ -81,7 +101,7 @@
     justify-content: center;
     width: 100%;
     flex-direction: column;
-    gap: 100px;
+    gap: 10px;
   }
   .menu{
     display: flex;
@@ -89,6 +109,8 @@
     height: 70px;
     font-family: poppin,Tahoma,Arial,\5FAE\8F6F\96C5\9ED1,sans-serif;
     font-size: 14px;
+    align-items: center;
+    justify-content: center;
   }
   .menu > a{
     display: block;
@@ -97,6 +119,8 @@
     height: 90px;
     text-align: center;
     font-size: 18px;
+    text-decoration: none;
+    color: black;
   }
   .view{
     width: 100%;
@@ -110,17 +134,14 @@
   }
   .active{
     background-color: #31c27c;
+    color: white;
   }
   .music_icon{
     
     width: 170px;
     height: 76px;
   }
-  .inputframe{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+
   .loginbutton {
   border: none;
   box-shadow: none;
@@ -132,8 +153,26 @@
     box-shadow: none;
     color: #31c27c;
   }
-  .vipbutton{
-    
+
+  .vip-button {
+    padding: 10px 20px;
+    height: 45px;
+    background-color: #31c27c;
+    border: none;
+  }
+  .buy-button {
+    padding: 10px 20px;
+    height: 45px;
+    background-color: white;
+    color: black;
+    border-color: black;
+
+  }
+
+  .custom-dropdown .el-dropdown-item {
+    height: 50px;
+    line-height: 50px;
+    padding: 0 20px;
   }
 
 </style>
